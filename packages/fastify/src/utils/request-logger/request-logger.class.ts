e9966cd3915cba764d3bd051703a29c1cf1942e7
Luis Hernán Paúl Ossandón <lhpaul11@gmail.com>
2025-05-13 16:45:51 -0400

    fix: Fixed problem when logging last step in 500 in rest api.

--- packages/fastify/src/utils/request-logger/request-logger.class.ts
+++ packages/fastify/src/utils/request-logger/request-logger.class.ts
@@ -10,9 +10,11 @@ export class RequestLogger implements ExecutionLogger {
   private _activeSteps: { [labe: string]: { initTime: number } } = {};
   private _logger: FastifyBaseLogger;
   private _lastStep: ExecutionStep = { id: '' };
-  constructor(options: { logger: FastifyBaseLogger }) {
+  private _parent?: RequestLogger;
+  constructor(options: { logger: FastifyBaseLogger; parent?: RequestLogger }) {
     this._logger = options.logger;
     this.initTime = new Date().getTime();
+    this._parent = options.parent;
   }
 
   get lastStep(): ExecutionStep {
@@ -38,7 +40,10 @@ export class RequestLogger implements ExecutionLogger {
   public silent = (data: any, message?: string) =>
     this._logger.silent(data, message);
   child(bindings: Bindings, options?: ChildLoggerOptions): FastifyBaseLogger {
-    return new RequestLogger({ logger: this._logger.child(bindings, options) });
+    return new RequestLogger({
+      logger: this._logger.child(bindings, options),
+      parent: this,
+    });
   }
   getTotalElapsedTime(): number {
     return new Date().getTime() - this.initTime;
@@ -51,30 +56,44 @@ export class RequestLogger implements ExecutionLogger {
     return new Date().getTime() - step.initTime;
   }
 
-  startStep(label: string, obfuscatedId?: string): void {
+  startStep(
+    label: string,
+    obfuscatedId?: string,
+    config?: { silent?: boolean },
+  ): void {
     const now = new Date().getTime();
     this._activeSteps[label] = { initTime: now };
-    this._logger.info(LOGS.STEP_START.logMessage(label), {
-      logId: LOGS.STEP_START.logId,
-      step: label,
-      totalElapsedTime: now - this.initTime,
-    });
+    if (!config?.silent) {
+      this._logger.info(LOGS.STEP_START.logMessage(label), {
+        logId: LOGS.STEP_START.logId,
+        step: label,
+        totalElapsedTime: now - this.initTime,
+      });
+    }
     this._lastStep = { id: label, obfuscatedId };
+    if (this._parent) {
+      this._parent.startStep(label, obfuscatedId, { silent: true });
+    }
   }
 
-  endStep(label: string): void {
+  endStep(label: string, config?: { silent?: boolean }): void {
     const step = this._activeSteps[label];
     if (!step) {
       return;
     }
     const now = new Date().getTime();
     const elapsedTimeFromPreviousStep = now - step.initTime;
-    this._logger.info(LOGS.STEP_END.logMessage(label), {
-      logId: LOGS.STEP_END.logId,
-      step: label,
-      elapsedTimeFromPreviousStep,
-      totalElapsedTime: now - this.initTime,
-    });
+    if (!config?.silent) {
+      this._logger.info(LOGS.STEP_END.logMessage(label), {
+        logId: LOGS.STEP_END.logId,
+        step: label,
+        elapsedTimeFromPreviousStep,
+        totalElapsedTime: now - this.initTime,
+      });
+    }
     delete this._activeSteps[label];
+    if (this._parent) {
+      this._parent.endStep(label, { silent: true });
+    }
   }
 }
