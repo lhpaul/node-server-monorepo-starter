1b29ccff18ec6a4d5e9dbac591d654d08a3225ee
Luis Hernán Paúl Ossandón <lhpaul11@gmail.com>
2025-06-09 19:02:26 -0400

    chore: modified subscriptions repository in order to use firestore.

--- packages/shared/src/repositories/subscriptions/subscriptions.repository.constants.ts
+++ packages/shared/src/repositories/subscriptions/subscriptions.repository.constants.ts
@@ -1,14 +1,1 @@
-export const ERROR_MESSAGES = {
-  COMPANY_NOT_FOUND: 'Company not found',
-} as const;
-
-export const MOCK_SUBSCRIPTIONS = [
-  {
-    id: '0',
-    companyId: '0',
-    startsAt: new Date('2024-01-01'),
-    endsAt: new Date('2024-12-31'),
-    createdAt: new Date('2024-01-01'),
-    updatedAt: new Date('2024-01-01'),
-  },
-]; 
+export const COLLECTION_PATH = 'companies/:companyId/subscriptions';
--- packages/shared/src/repositories/subscriptions/subscriptions.repository.ts
+++ packages/shared/src/repositories/subscriptions/subscriptions.repository.ts
@@ -1,44 +1,19 @@
-import { ExecutionLogger } from '../../definitions';
 import { Subscription } from '../../domain/models/subscription.model';
-import { InMemoryRepository } from '../../utils/repositories/in-memory-repository.class';
-import { RepositoryError, RepositoryErrorCode } from '../../utils/repositories/repositories.errors';
-import { CompaniesRepository } from '../companies/companies.repository';
-import { ERROR_MESSAGES, MOCK_SUBSCRIPTIONS } from './subscriptions.repository.constants';
-import {
-  CreateSubscriptionInput,
-  GetSubscriptionsQuery,
-  UpdateSubscriptionInput,
-} from './subscriptions.repository.interfaces';
+import { FirestoreCollectionRepository } from '../../utils/firestore/firestore-collection-repository.class';
+import { COLLECTION_PATH } from './subscriptions.repository.constants';
+import { CreateSubscriptionInput, GetSubscriptionsQuery, UpdateSubscriptionInput } from './subscriptions.repository.interfaces';
 
-export class SubscriptionsRepository extends InMemoryRepository<Subscription, CreateSubscriptionInput, UpdateSubscriptionInput, GetSubscriptionsQuery> {
+export class SubscriptionsRepository extends FirestoreCollectionRepository<Subscription, CreateSubscriptionInput, UpdateSubscriptionInput, GetSubscriptionsQuery> {
   private static instance: SubscriptionsRepository;
-
   public static getInstance(): SubscriptionsRepository {
     if (!SubscriptionsRepository.instance) {
-      SubscriptionsRepository.instance = new SubscriptionsRepository(MOCK_SUBSCRIPTIONS);
+      SubscriptionsRepository.instance = new SubscriptionsRepository();
     }
     return SubscriptionsRepository.instance;
   }
-
-  /**
-   * Creates a new subscription
-   * @param data - The data to create the new subscription with
-   * @param logger - Logger instance for tracking execution
-   * @returns Promise resolving to the ID of the created subscription
-   * @throws RepositoryError with code {@link RepositoryErrorCode.RELATED_DOCUMENT_NOT_FOUND} if the related company is not found
-   */
-  async createDocument(data: CreateSubscriptionInput, logger: ExecutionLogger): Promise<string> {
-    const { companyId } = data;
-    const company = await CompaniesRepository.getInstance().getDocument(companyId, logger);
-    if (!company) {
-      throw(new RepositoryError({
-        code: RepositoryErrorCode.RELATED_DOCUMENT_NOT_FOUND,
-        message: ERROR_MESSAGES.COMPANY_NOT_FOUND,
-        data: {
-          companyId,
-        },
-      }))
-    }
-    return super.createDocument(data, logger);
+  constructor() {
+    super({
+      collectionPath: COLLECTION_PATH
+    });
   }
-} 
+}
