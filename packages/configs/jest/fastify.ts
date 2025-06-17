2318422e63a2961ce9d031ed745e95103d5d2a09
Luis Hernán Paúl Ossandón <lhpaul11@gmail.com>
2025-06-17 17:12:44 -0400

    refactor: changed some double quotes to single quotes.

--- packages/configs/jest/fastify.ts
+++ packages/configs/jest/fastify.ts
@@ -1,11 +1,11 @@
-import type { Config } from "jest";
-import { config as baseConfig } from "./base";
+import type { Config } from 'jest';
+import { config as baseConfig } from './base';
 
 export const config = {
   ...baseConfig,
   collectCoverageFrom: [
     ...baseConfig.collectCoverageFrom,
-    "!src/routes.ts",
-    "!src/server.ts",
+    '!src/routes.ts',
+    '!src/server.ts',
   ],
 } as const satisfies Config;
