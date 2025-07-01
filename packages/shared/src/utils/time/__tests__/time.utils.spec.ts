eee53f08be8cc461cc709b10b31e55e1ec4a0920
Luis Hernán Paúl Ossandón <lhpaul11@gmail.com>
2025-07-01 10:28:58 -0400

    chore: Improved unit tests for time utils.

--- packages/shared/src/utils/time/__tests__/time.utils.spec.ts
+++ packages/shared/src/utils/time/__tests__/time.utils.spec.ts
@@ -1,29 +1,38 @@
 import { wait } from '../time.utils';
 
 describe(wait.name, () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+
+  afterEach(() => {
+    jest.useRealTimers();
+  });
   it('should resolve after the specified milliseconds', async () => {
     const startTime = Date.now();
     const waitTime = 100;
 
-    await wait(waitTime);
+    await Promise.all([
+      wait(waitTime),
+      jest.runAllTimers(),
+    ]);
 
     const endTime = Date.now();
     const elapsedTime = endTime - startTime;
-
-    // Allow for a small margin of error (±10ms) due to system timing variations
-    expect(elapsedTime).toBeGreaterThanOrEqual(waitTime - 10);
-    expect(elapsedTime).toBeLessThanOrEqual(waitTime + 10);
+    expect(elapsedTime).toBe(waitTime);
   });
 
   it('should resolve immediately when wait time is 0', async () => {
     const startTime = Date.now();
     
-    await wait(0);
-    
+    await Promise.all([
+      wait(0),
+      jest.runAllTimers(),
+    ]);
+
     const endTime = Date.now();
     const elapsedTime = endTime - startTime;
 
-    // Should be very close to 0, but allow for a small margin
-    expect(elapsedTime).toBeLessThan(10);
+    expect(elapsedTime).toBe(0);
   });
 });
