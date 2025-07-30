b873462f9d84a395cf04ca4cda0e37ef19e0e501
Luis Hernán Paúl Ossandón <lhpaul11@gmail.com>
2025-07-30 17:59:31 -0400

    feat: transaction-categories endpoint for public api.

---
+++ apps/public-api/src/endpoints/transaction-categories/handlers/list/transaction-categories.list.handler.interfaces.ts
@@ -0,0 +1,3 @@
+export interface ListTransactionCategoriesQuery {
+  type?: 'income' | 'expense';
+} 
--- apps/public-api/src/routes.ts
+++ apps/public-api/src/routes.ts
@@ -2,11 +2,13 @@ import { FastifyInstance, RouteOptions } from 'fastify';
 
 import { authEndpointsBuilder } from './endpoints/auth/auth.endpoints';
 import { companiesEndpointsBuilder } from './endpoints/companies/companies.endpoints';
+import { transactionCategoriesEndpointsBuilder } from './endpoints/transaction-categories/transaction-categories.endpoints';
 
 export const routesBuilder = (server: FastifyInstance): RouteOptions[] => {
   return [
     ...authEndpointsBuilder(server),
-    ...companiesEndpointsBuilder(server)
+    ...companiesEndpointsBuilder(server),
+    ...transactionCategoriesEndpointsBuilder(server)
   ];
 };
 
