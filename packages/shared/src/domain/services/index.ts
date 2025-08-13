e1f1541c7c0ee61e5e79f805b6f02720de42fb72
Luis Hernán Paúl Ossandón <lhpaul11@gmail.com>
2025-08-13 17:07:49 -0400

    feat: Added service for user company relations.

--- packages/shared/src/domain/services/index.ts
+++ packages/shared/src/domain/services/index.ts
@@ -6,4 +6,5 @@ export * from './private-keys/index.js';
 export * from './subscriptions/index.js';
 export * from './transaction-categories/index.js';
 export * from './transactions/index.js';
+export * from './user-company-relations/index.js';
 export * from './users/index.js';
---
+++ packages/shared/src/domain/services/user-company-relations/user-company-relations.service.constants.ts
@@ -0,0 +1,2 @@
+// This file contains constants for the user-company-relations service
+// Currently empty as the service doesn't have complex operations like the companies service
